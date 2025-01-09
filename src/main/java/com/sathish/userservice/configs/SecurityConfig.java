fed2544621046f95b10152bb40bbda5e0226bc09
Sathish474 <sathishchowdary474@gmail.com>
2025-01-09 13:29:46 -0600

    Configure security settings to ensure proper redirection to login page for unauthenticated users and add Eureka client configuration

--- src/main/java/com/sathish/userservice/configs/SecurityConfig.java
+++ src/main/java/com/sathish/userservice/configs/SecurityConfig.java
@@ -2,14 +2,16 @@ package com.sathish.userservice.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+   // @Bean
+    //@Order(3)
+    public SecurityFilterChain securityFilterMethod(HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(authorize -> authorize
